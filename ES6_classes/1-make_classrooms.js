5a425e2a4c014a2c405c1acb3184f2ff8a94738b
zack6yuan <zack6yuan@gmail.com>
2025-03-03 13:07:19 -0600

    update task 1.

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,2 +1,11 @@
 import ClassRoom from '0-classroom';
-export default function initializeRooms()
+
+export default function initializeRooms() {
+  const array = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+
+  return array;
+}
