d79210b17634fc47d2ffc332847db5f48ab5b5eb
zack6yuan <zack6yuan@gmail.com>
2025-03-02 23:49:00 -0600

    update task 8.

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -5,9 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [income]: "income",
-    [gdp]: "gdp",
-    [capita]: "capita"
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 
   return budget;
