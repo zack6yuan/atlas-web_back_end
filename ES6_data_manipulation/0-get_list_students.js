3cbc991cc9a9aa35cef073f2938ad3b993faafbb
zack6yuan <zack6yuan@gmail.com>
2025-03-03 00:11:38 -0600

    update task 0.

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const array = [
+    {name: "Guillaume", id: "1", location: "San Francisco"},
+    {name: "James", id: "2", location: "Columbia"},
+    {name: "Serena", id: "5", location: "San Francisco"}
+  ];
+
+  return array;
+}
