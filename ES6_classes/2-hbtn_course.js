1fbd61cfc6f84a781c7fce5c51e291d2e09e7de2
zack6yuan <zack6yuan@gmail.com>
2025-03-03 13:28:38 -0600

    update task 2.

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+  /* Attributes
+  name: string
+  length: number
+  students: array of strings */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // Setter for name
+  set name(name) {
+    if (typeof name !== "string") {
+      throw new TypeError("name must be a string");
+    }
+    this._name = name;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for length
+  set length(length) {
+    if (typeof length !== "number") {
+      throw new TypeError("length must be a number");
+    }
+    this._length = length;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for students
+  set students(students) {
+    if (typeof students !== "string") {
+      throw new TypeError("students must be an array of strings");
+    }
+    this._students = students;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+}
