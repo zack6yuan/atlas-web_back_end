6dd05a4ddbce13dbb0dec06706706783da0b0c6f
zack6yuan <zack6yuan@gmail.com>
2025-03-03 13:00:26 -0600

    merge.

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
 import ClassRoom from '0-classroom';
 export default function initializeRooms()
-=======
-export default class ClassRoom {
-  constructor(InitializeRooms) {
-    this.InitializeRooms
-  }
-}
->>>>>>> 3cbc991cc9a9aa35cef073f2938ad3b993faafbb
