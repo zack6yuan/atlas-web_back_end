53071a33246449a665fde02f96dbdc1affadb3bd
zack6yuan <zack6yuan@gmail.com>
2025-03-03 23:31:13 -0600

    mabye?

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    this.name = name // string
-    this.code = code //string
+    this._name = name // string
+    this._code = code //string
   }
 
   AirportDetails() {
-    return `_name: '${this.name}', _code: '${this.code}'`; // not correct
+    return `_name: '${this._name}', _code: '${this._code}'`; // not correct
   }
 }
---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,6 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size; // number
+    this.string = string // string
+  }
+}
