b0714d1c6ec5c8c5f13e95efde61198f6cafe34d
zack6yuan <zack6yuan@gmail.com>
2025-03-03 23:59:42 -0600

    use for of.

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
+  for (const idx of array) {
+    const value = array[idx];
     array[idx] = appendString + value;
   }
 
