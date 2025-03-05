0f770671e81d0ffaaad517eca18378e4cec3ddd0
zack6yuan <zack6yuan@gmail.com>
2025-03-04 22:54:53 -0600

    remove period.

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -28,6 +28,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
