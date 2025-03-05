db2718124803c37f71473216816c9f6461d23519
zack6yuan <zack6yuan@gmail.com>
2025-03-04 22:19:33 -0600

    format?

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,5 +1,8 @@
 export default function returnHowManyArguments(...args) {
   let total = 0;
-  for (let arg of args) total += arg;
+  for (let arg of args) {
+    total += arg;
+  }
+
   return total.length;
 }
