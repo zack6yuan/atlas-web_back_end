3d5402d93d34bf49cae0e668ad918e2e0fdcc608
zack6yuan <zack6yuan@gmail.com>
2025-03-04 23:12:58 -0600

    added a setter.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -7,6 +7,13 @@ export default class Building {
     return `Class extending Building must override evacuationWarningMessage`
   }
 
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this.sqft = sqft;
+  }
+
   get sqft() {
     return this._sqft;
   }
