59f27e19b5a3d2f8ca301fd4feb48beb8269c36d
zack6yuan <zack6yuan@gmail.com>
2025-03-04 22:51:32 -0600

    update task 6.

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,7 +1,7 @@
 import Building from './5-building';
-export default class SkyHighBuilding {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    this.sqft = sqft; // number: assigned to parent class Building
+    super(sqft); // number: assigned to parent class Building
     this.floors = floors; // number
   }
 
