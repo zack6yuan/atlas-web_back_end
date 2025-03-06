91e726f8c3268e7e6c29e764502ee698b23b43f7
zack6yuan <zack6yuan@gmail.com>
2025-03-06 19:58:33 +0000

    remove.

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,23 +3,7 @@ export default function getListStudentIds(array) {
     newArray = [];
     return newArray;
   }
-  const array = [
-    {
-      firstName: 'Guillaume',
-      id: 1,
-      location: 'San Francisco',
-    },
-    {
-      firstName: 'James',
-      id: 2,
-      location: 'Columbia',
-    },
-    {
-      firstName: 'Serena',
-      id: 5,
-      location: 'San Francisco',
-    },
-  ];
+
   const arrayIds = array.map((id) => `${id}`
   )
 }
