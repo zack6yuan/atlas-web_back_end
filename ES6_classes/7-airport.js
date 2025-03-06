ad20a578ac90ef4d9203e6882a2b1517b41b815d
zack6yuan <zack6yuan@gmail.com>
2025-03-05 23:50:44 -0600

    update 7 and 9.

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,10 +1,6 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = name // string
-    this._code = code //string
-  }
-
-  AirportDetails() {
-    return `_name: '${this._name}', _code: '${this._code}'`; // not correct
+    this._name = name;
+    this._code = code;
   }
 }
--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,3 +1,6 @@
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -25,7 +28,7 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this.holbertonClass;
   }
 
   get fullStudentDescription() {
@@ -33,15 +36,9 @@ export class StudentHolberton {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 export const listOfStudents = [student1, student2, student3, student4, student5];
-
-// need to fix this code to make it work
