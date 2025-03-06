edf9b28af732d459f4b7b5f1e38bd204dbbcd3be
zack6yuan <zack6yuan@gmail.com>
2025-03-06 17:37:57 +0000

    update task 2.

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      const object = {
+        status: 200,
+        body: 'success',
+      }
+      resolve(object);
+    } else {
+      reject(new Error)
+    }
+  })
+}
