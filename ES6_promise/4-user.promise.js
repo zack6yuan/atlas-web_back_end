4cc736d099fa6247f1adda656a93c25ceeffadf3
zack6yuan <zack6yuan@gmail.com>
2025-03-07 19:05:21 +0000

    update task 4.

---
+++ ES6_promise/4-user.promise.js
@@ -0,0 +1,13 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    if (firstName && lastName) {
+      const names = {
+        firstName: firstName,
+        lastName: lastName,
+      }
+      resolve(names);
+    } else {
+      reject(new Error('firstName and lastName are not valid.'))
+    }
+  })
+}
