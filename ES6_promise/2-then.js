f89d3cfd5e05341752e6ce4ae0813b76074950d1
zack6yuan <zack6yuan@gmail.com>
2025-03-09 00:25:45 +0000

    update.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    if (promise(resolve) == true) {
-     return {
-      status: 200,
-      body: 'success',
-     };
-     console.log('got a response from the API')
+    if (promise) {
+      const object = {
+        status: 200,
+        body: 'success',
+      }
+      resolve(object);
     } else {
-      reject(new Error(''))
-      const errorObject = [];
-
-      return errorObject;
+      reject(new Error);
     };
   })
 }
