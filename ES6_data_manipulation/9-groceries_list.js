1e0247072c3e89d973cc1c7ac01948979e35d448
zack6yuan <zack6yuan@gmail.com>
2025-03-08 19:53:38 -0600

    update task 9.

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,10 @@
-export default function groceriesList {
-  
+export default function groceriesList() {
+  const groceriesMap = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  }
+   return groceriesList.map(groceriesMap);
 }
