5170ecf73e7e9489b442b9081e709557842f6ec4
zack6yuan <zack6yuan@gmail.com>
2025-03-08 20:38:19 -0600

    temporarily fixed.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft; // number
+    this.sqft = sqft; // number
   }
 
-class extendingClass extends Building {
-  constructor() {
-    super(sqft);
-  }
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
-};
