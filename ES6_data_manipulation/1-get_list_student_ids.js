20a9e5f524ee3f4e3f8dc3f25ed1c92460b89e3d
zack6yuan <zack6yuan@gmail.com>
2025-03-08 19:38:02 -0600

    wrong function.

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,8 @@
+import getListStudents from "./0-get_list_students"
+
 export default function getListStudentIds(array) {
   if (typeof array != 'array') {
     const newArray = []
-    return newArray.map(getListStudentIds)
+    return newArray.map(getListStudents)
   }
 }
