cbfed017ae2c4a0a2b24b50533fa49f8da9eb6fe
zack6yuan <zack6yuan@gmail.com>
2025-03-08 20:45:03 -0600

    updates.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,6 +3,14 @@ export default class Building {
     this.sqft = sqft; // number
   }
 
+  get sqft() {
+    return this.sqft;
+  }
+
+  if (evacuationWarningMessage not instanceof Building) {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
