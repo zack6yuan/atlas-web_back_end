95b9ed19369e9030701f527cd710af1d13ea93ff
zack6yuan <zack6yuan@gmail.com>
2025-03-08 21:39:13 -0600

    update.

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,7 @@
 export default function createReportObject(employeesList) {
   const object = {};
-  const objectKey = object.key = "allEmployees";
+  const objectKey = "allEmployees";
+  object.objectKey = employeesList;
 
   return (objectKey, getNumberOfDepartments());
 }
