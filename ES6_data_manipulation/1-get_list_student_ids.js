60f582109921e8b424b1726e94656ddb4d8a3a6a
zack6yuan <zack6yuan@gmail.com>
2025-03-11 15:25:25 -0500

    eslint.

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,11 @@
 import getListStudents from "./0-get_list_students"
 
 export default function getListStudentIds(objectsArray) {
+  /* eslint-disable no-unused-vars */
   // if not an array, return an empty array
   if (!Array.isArray(objectsArray)) {
     return [];
   }
   // use map function
-  return objectsArray.map(obj => obj.id);
+  return objectsArray.map(obj => (obj.id));
 }
