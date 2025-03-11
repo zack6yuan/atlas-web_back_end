ec675e526c0b8d235aa1095303838724cb2ba7ef
zack6yuan <zack6yuan@gmail.com>
2025-03-11 15:23:22 -0500

    update task 1.

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,10 @@
-export default function getListStudentIds {
-  
+import getListStudents from "./0-get_list_students"
+
+export default function getListStudentIds(objectsArray) {
+  // if not an array, return an empty array
+  if (!Array.isArray(objectsArray)) {
+    return [];
+  }
+  // use map function
+  return objectsArray.map(obj => obj.id);
 }
