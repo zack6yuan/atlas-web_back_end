091d60b2e57000600ad53e31d306b53aaea091f3
zack6yuan <zack6yuan@gmail.com>
2025-03-10 22:56:35 -0500

    start.

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,9 @@
 import getListStudents from "./0-get_list_students"
 
-export default function getListStudentIds(array) {
-  if (Array.isArray(array)) {
-    const newArray = []
-    return newArray.map(getListStudents(array[1]));
+export default function getListStudentIds(objectsArray) {
+  if (!Array.isArray(objectsArray)) {
+    return [];
   } else {
-    throw (new TypeError('not an array'))
-  };
+    return objectsArray.map();
+  }
 }
