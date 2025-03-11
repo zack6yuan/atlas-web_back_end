2c7c7322facf7250c7488ddbb5f6230aa73438d5
zack6yuan <zack6yuan@gmail.com>
2025-03-11 15:34:28 -0500

    start task 2.

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,7 @@
-export default function getStudentsByLocation {
-  
+export default function getStudentsByLocation(students, city) {
+  // if not an array, return an empty array
+  if (!Array.isArray(objectsArray)) {
+    return [];
+  }
+  filteredStudents = students.filter()
 }
