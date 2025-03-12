375a11e27bef361d38fb51157c0a70f10d507f8f
zack6yuan <zack6yuan@gmail.com>
2025-03-12 16:58:59 -0500

    remove single quotes.

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
   const items = [
-    ['Apples', '10'],
-    ['Tomatoes', '10'],
-    ['Pasta', '1'],
-    ['Rice', '1'],
-    ['Banana', '5'],
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
   ];
 
   return new Map(items);
