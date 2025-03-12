0225fc3351af588a0221917dd14e4a110e7b7491
zack6yuan <zack6yuan@gmail.com>
2025-03-12 15:51:00 -0500

    update task 9.

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,12 +1,15 @@
 export default function guardrail(mathFunction) {
+  // array to push values to
   const queue = [];
-  .try(mathFunction())
-  .then(() => {
-    queue.push()
-  })
-
-  // When the mathFunction function is executed, the value 
-  // returned by the function should be appended to the queue. 
-  // If this function throws an error, the error message should 
-  // be appended to the queue. In every case, the message Guardrail 
-  // was processed should be added to the queue.
+  const message = 'Guardrail was processed'
+  try {
+    const value = mathFunction()
+    // append value to array
+    queue.push(value, message);
+  } catch(error) {
+    // push error message
+    queue.push('Error', message)
+  } finally {
+    queue.push(message)
+  }
+}
