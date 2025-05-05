4e0e7a8900c8db847b7c4f5caedddc9c91652a4d
zack6yuan <zack6yuan@gmail.com>
2025-05-05 08:11:17 -0500

    change type to list.

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # Method: Returns the sum of list elements as a float
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: list) -> float:
     return (sum(input_list))
