23d8acc49947c1cdc3522f62968e87c70a7132c6
zack6yuan <zack6yuan@gmail.com>
2025-05-06 16:28:55 +0000

    modify executables and update task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Method: Returns the sum of a mixed list as a float
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    return (sum(mxd_lst))
