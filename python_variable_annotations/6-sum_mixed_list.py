29873b9db7c618086acd8d2c722e8a7e381361ff
zack6yuan <zack6yuan@gmail.com>
2025-05-06 16:31:58 +0000

    Learned what Union is.

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Method: Returns the sum of a mixed list as a float
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     return (sum(mxd_lst))
