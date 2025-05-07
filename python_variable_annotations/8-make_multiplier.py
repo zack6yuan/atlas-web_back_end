82d6d26899fb4408c3c18123db890b7e70d8d9dc
zack6yuan <zack6yuan@gmail.com>
2025-05-07 21:26:35 +0000

    update.

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Handles complex types with functions
+from typing import Union
+
+
+def make_multiplier(multiplier: float):
+    def floatMultiplier():
+        return (fun * multiplier)
+
+        # im confused
