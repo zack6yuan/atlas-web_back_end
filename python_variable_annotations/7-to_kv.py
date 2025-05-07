33c5e1ca8468a079b22ea1352ae2efab40ee7731
zack6yuan <zack6yuan@gmail.com>
2025-05-07 21:11:32 +0000

    fixed it.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Method: Handle complex types
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: [Union[int, float]]) -> tuple:
+def to_kv(k: str, v: [Union[int, float]]) -> Tuple[str, Union[int, float]]:
     value2 = v * v
     return (k, value2)
