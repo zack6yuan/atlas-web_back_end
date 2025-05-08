335da39c097d3e274363b1eb3fc2a4d9eeb4283f
zack6yuan <zack6yuan@gmail.com>
2025-05-08 16:22:56 +0000

    update documentation.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-# Method: Handle complex types
+"""
+Type-annotated function that takes values and returns a tuple
+"""
 from typing import Union, Tuple
 
 
 # Give a type hint to the tuple that will be returned
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Parameters:
+    k (str) --> string value (first element of the tuple)
+    v (int / float) --> int / float value
+
+    Returns:
+    Tuple --> Values: (k(str), v(square value))
+    """
     value2 = v * v
     return (k, value2)
