b122b2658314e76ad278f3f718eb795d40f08dbc
zack6yuan <zack6yuan@gmail.com>
2025-05-08 16:29:16 +0000

    revert.

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-# Handles complex types with functions
+"""
+Type-annotated function that takes a float
+Returns a function taht multiplies a float by multiplier
+"""
 from typing import Union
 
 
 def make_multiplier(multiplier: float):
-    def floatMultiplier():
-        return (fun * multiplier)
+    """
+    Parameters:
+    multiplier (float) --> float value
 
-        # im confused
+    Returns:
+    New Function that multiples a float by multiplier
+    """
