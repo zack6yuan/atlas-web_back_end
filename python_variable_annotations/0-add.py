5a17d1b0ee41bfae70bbb743611f31b82975b8a0
zack6yuan <zack6yuan@gmail.com>
2025-05-08 15:37:22 +0000

    update documentation.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 # Method: Return sum using type annotation
 def add(a: float, b: float) -> float:
-    # Return as float
+    """
+    Parameters:
+    a (float) --> First argument
+    b (float) --> Second argument
+
+    Returns:
+    The sum of the two arguments as a float value
+    """
     return float(a + b)
