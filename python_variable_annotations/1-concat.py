b7adffb5d2877d84b5b006102cb5e1a0a4739e24
zack6yuan <zack6yuan@gmail.com>
2025-05-08 16:09:30 +0000

    update documentation.

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
-# Method: Concatenate Strings
+"""
+A function that takes a string and concatenates it
+"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """
+    Parameters:
+    str1 (string) --> the first string
+    str2 (string) --> the second string
+
+    Returns:
+    The concatenation of the two strings
+    """
     return (str1 + str2)
