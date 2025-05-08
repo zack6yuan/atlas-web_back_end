aed82ffd91f37e134beb46bdb3224abbf8d9af40
zack6yuan <zack6yuan@gmail.com>
2025-05-08 16:15:56 +0000

    update documentation.

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
-# Method: Define and annotate variables with specified values
+"""
+Annotate variables with specified values
+
+Parameters:
+a (integer) --> value: 1
+pi (float) --> value: 3.14
+i_understand_annotations (boolean) --> value: True
+school (string) --> value: "Holberton"
+
+Returns:
+Values according to format
+"""
+
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
