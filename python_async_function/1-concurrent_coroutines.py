5ee4282ee4aded3435eaa3b0ed7fd31e5ed15804
zack6yuan <zack6yuan@gmail.com>
2025-05-08 21:52:10 +0000

    updating.

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Asynchronous Coroutine that takes in two integer arguments
+"""
+import asyncio
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Parameters:
+    n (int) --> the number of times wait_random is executed
+    max_delay (int) --> max delay for the coroutine
+
+    Returns:
+    List of all the delays in float values in ascending order
+    """
+    value = random.uniform(0, max_delay)
+    await asyncio.sleep(value)
+    return value
