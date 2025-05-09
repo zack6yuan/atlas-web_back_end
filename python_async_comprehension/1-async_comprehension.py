d84047ca04c64d5ebad2657389cd3c5de744fa17
zack6yuan <zack6yuan@gmail.com>
2025-05-09 16:07:19 +0000

    update task 1.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> float
     """
     Parameters: None
 
