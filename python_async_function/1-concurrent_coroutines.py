bb64d4ee82d696c47f49eefd2ae4741baf8a82fa
zack6yuan <zack6yuan@gmail.com>
2025-05-09 14:57:53 +0000

    update task 1.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@ Asynchronous Coroutine that takes in two integer arguments
 """
 import asyncio
 import random
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Parameters:
     n (int) --> the number of times wait_random is executed
