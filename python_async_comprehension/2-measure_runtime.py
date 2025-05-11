e6b58d466df638a64335a2e42f95c59ba79937ca
zack6yuan <zack6yuan@gmail.com>
2025-05-11 16:34:28 +0000

    update task 2.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -11,11 +11,27 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+    Parameters: None
+
+    Methods:
+    While async_comprehension is executed, start a time counter and log
+    asyncio.gather --> execute multiple coroutines, and wait for them to complete
+
+    Returns:
+    Total time for the execution of the coroutine
+    """
+    start = time.perf_counter()  # Start time marker
+
     process1 = async_comprehension()
     process2 = async_comprehension()
     process3 = async_comprehension()
     process4 = async_comprehension()
 
-    process_results = await asyncio.gather(process1, process2, process3, process4)
+    await asyncio.gather(process1, process2, process3, process4)
+
+    end = time.perf_counter()  # End time marker
+
+    process_results = end - start  # Total time calculation
 
     return process_results
