898acb4ff09393ee72342d578efd1a2f2eeae04d
zack6yuan <zack6yuan@gmail.com>
2025-05-11 16:43:35 +0000

    update task 2 pycodestyle.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -15,14 +15,22 @@ async def measure_runtime() -> float:
     Parameters: None
 
     Methods:
-    While async_comprehension is executed, start a time counter and log
-    asyncio.gather --> execute multiple coroutines, and wait for them to complete
+    While async_comprehension is executed,
+    start a time counter and log
+    
+    asyncio.gather --> execute multiple coroutines,
+    and wait for them to complete
 
     Returns:
     Total time for the execution of the coroutine
     """
     start = time.perf_counter()  # Start time marker
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
     end = time.perf_counter()  # End time marker
 
     process_results = end - start  # Total time calculation
