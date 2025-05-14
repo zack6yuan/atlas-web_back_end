92fe88843655585829b81b90c841279470511b42
zack6yuan <zack6yuan@gmail.com>
2025-05-14 21:16:14 +0000

    update task 0.

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+class BasicCache(BaseCaching)
+    def __init__(self):
+        self.cache_data = {}
+        
+    def put(self, key, item):
+        """
+        Arguments:
+        self --> instance of the class
+        key --> unique identifier
+        item --> key-value pair
+        
+        Methods:
+        Assigns key item value to the dictionary
+        key || item (none) --> nothing executed
+        """
+        
+        
+    def get(self, key):
+        """
+        Arguments:
+        self --> instance of the class
+        key --> uniqie identifier
+        
+        Methods:
+        key || item (none) --> nothing executed
+        
+        Returns:
+        Value in self.cache_data linked to key
+        """
