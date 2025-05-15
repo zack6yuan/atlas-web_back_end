5be2e5aa82aa16bda06c787782b8cb95e2d353d8
zack6yuan <zack6yuan@gmail.com>
2025-05-15 00:00:31 -0500

    updated starter files.

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-def filter_datum(fields, redaction, message, separator):
+def hash_password(password: str):
+    """
+    Arguments:
+    password (str) ---> user's password
     
+    Returns:
+    Salted, hashed password, which is a byte string
+    """
---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def filter_datum(fields: str, redaction: str, message: str, separator: str):
+    """
+    Arguments:
+    fields (list(str)) --> all fields to obfuscate
+    redaction (str) --> what the fiield will be obfuscated by
+    message (str) --> log time
+    separator(str) --> character separating all log time fields
+    """
