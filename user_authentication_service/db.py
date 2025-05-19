8b8e381bdbc57a120366d7262a08377cdd29531a
zack6yuan <zack6yuan@gmail.com>
2025-05-19 03:27:36 +0000

    added self.

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -29,8 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
     
-    def add_user(email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         new_user = User(email=email, hashed_password=hashed_password)
         DBSession.add(new_user)
         return new_user
-        
