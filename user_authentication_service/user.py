9ab58c93b1f2a2e2f8eff91812ae762904db0fac
zack6yuan <zack6yuan@gmail.com>
2025-05-19 02:58:24 +0000

    updates.

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
-import sqlalchemy
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-class
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
