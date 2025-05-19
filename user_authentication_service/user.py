80ec077fb566cdc05cd39f8ea05589201f7d2553
zack6yuan <zack6yuan@gmail.com>
2025-05-19 03:01:31 +0000

    update doc.

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
+    """ User Class """
     __tablename__ = 'users'
     
     id = Column(Integer, primary_key=True)
