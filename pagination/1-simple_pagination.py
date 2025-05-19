862cc7f2ea2e915e94e2c8f7ec4f533721e2bcd6
zack6yuan <zack6yuan@gmail.com>
2025-05-19 17:44:23 +0000

    upadte task 1.

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 """
-The code from the previous task will be put here
+Simple helper function
 """
-def get_page(page: int = 1, page_size: int = 10):
+from typing import Tuple
+import csv
+import math
+from typing import List
+
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
     """
-    Arguments:
-    page (int) --> pagination parameter / default value = 1
-    page_size (int) --> pagination parameter / default value = 10
-    
-    Methods:
-    Verify both arguments are > 0 (assert)
-    Find correct indexes to paginate the dataset
-    
-    Returns:
-    The appropriate page of the dataset
-    If input arguments are out of range, return empty list
-    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and (page > 0)
+        assert isinstance(page_size, int) and (page_size > 0)
+        
