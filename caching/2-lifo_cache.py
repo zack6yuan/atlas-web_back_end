b25bae063a237d2e72cc7ec33983cf82b4fd9e52
zack6yuan <zack6yuan@gmail.com>
2025-05-20 17:31:30 +0000

    update task 2.

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -9,14 +9,13 @@ class LIFOCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.cache_data = {}
         
     def put(self, key, item):
         """
         Arguments:
         self --> instance of the class
         key --> unique identifier
-        item --> key-value pair
+        item --> key- value pair
         
         Methods:
         Assigns key item value to the dictionary
@@ -24,28 +23,27 @@ class LIFOCache(BaseCaching):
         if num(items) of self.cache_data > BaseCaching.MAX_ITEMS...
         Discard last item and print according to format
         """
-        if key is not None and item is not None:
+        if key is None or item is None:
+            return
+        if item not in self.cache_data:
             self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            self.cache_data.pop(-1)
-            print("DISCARD: {}\n".format(key))
-                    
-        
+            last_item = self.cache_data.popitem()
+            print("DISCARD: {}".format(last_item[0]))
+    
     def get(self, key):
         """
         Arguments:
         self --> instance of the class
         key --> uniqie identifier
-        
+
         Methods:
         key || item (none) --> nothing executed
-        
+
         Returns:
         Value in self.cache_data linked to key
         key || item (none) --> return None
         """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        
         if key is None or key not in self.cache_data:
             return None
+        return self.cache_data[key]
