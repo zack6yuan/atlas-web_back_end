9d6fe491dcb227298133aa2f76c8f8c2036fa860
zack6yuan <zack6yuan@gmail.com>
2025-05-20 17:13:39 +0000

    whitespace.

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -9,14 +9,14 @@ class FIFOCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-            
+
     def put(self, key, item):
         """
         Arguments:
         self --> instance of the class
         key --> unique identifier
         item --> key-value pair
-        
+
         Methods:
         Assigns key item value to the dictionary
         key || item (none) --> nothing executed
@@ -32,17 +32,16 @@ class FIFOCache(BaseCaching):
             first_item = next(iter(self.cache_data))
             self.cache_data.pop(first_item)
             print("DISCARD: {}".format(first_item))
- 
 
     def get(self, key):
         """
         Arguments:
         self --> instance of the class
         key --> uniqie identifier
-        
+
         Methods:
         key || item (none) --> nothing executed
-        
+
         Returns:
         Value in self.cache_data linked to key
         key || item (none) --> return None
