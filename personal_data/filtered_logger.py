39d3d33e5c9163aa1f460c2fa72ac96413d228f3
zack6yuan <zack6yuan@gmail.com>
2025-05-20 00:04:08 +0000

    remove import.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import re
-from typing import List, String
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
