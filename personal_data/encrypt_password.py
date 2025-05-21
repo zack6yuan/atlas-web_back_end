0542768efaef41b6fe28d6c6c87db36594c068d0
zack6yuan <zack6yuan@gmail.com>
2025-05-21 19:34:49 +0000

    update doc.

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -23,7 +23,20 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(hashed_pwd, salt)
     return hashed
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Arguments:
+    hashed_password --> the previously hashed password
+    password --> the new password from the user as input
+
+    Methods:
+    Convert the new password to utf-8 to pass to bcrypt
+    Use checkpw to provided password to the hashed password
+
+    Returns:
+    The check for the password, as a Boolean (True or False)
+    """
     converted_pwd = password.encode('utf-8')
     check = bcrypt.checkpw(converted_pwd, hashed_password)
     return check
