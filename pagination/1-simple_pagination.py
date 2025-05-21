d7635a14f386916240c9b1eb31f316dcb49dad98
zack6yuan <zack6yuan@gmail.com>
2025-05-21 19:02:18 +0000

    updates.

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,4 +33,3 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int) and (page > 0)
         assert isinstance(page_size, int) and (page_size > 0)
-        
