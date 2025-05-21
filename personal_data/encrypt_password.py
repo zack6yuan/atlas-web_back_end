8a5405cbe67937e8ff9054383cd9a454d7a5a4e3
zack6yuan <zack6yuan@gmail.com>
2025-05-21 19:25:37 +0000

    update encrypt

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
-def hash_password(password: str):
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
     """
     Arguments:
     password (str) ---> user's password
     
+    Methods:
+    Encode the password to UTF-8 to pass to hashpw
+    Generate a salt ---> data added to password before modification
+    Pass the password to hashpw with the salt
+    
     Returns:
     Salted, hashed password, which is a byte string
     """
+    hashed_pwd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(hashed_pwd, salt)
+    return hashed
