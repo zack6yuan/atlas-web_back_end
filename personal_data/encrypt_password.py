a3f8a4e2e2c1257094d0bc98185881f398801baa
zack6yuan <zack6yuan@gmail.com>
2025-05-21 19:26:06 +0000

    whitespace.

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,12 +6,12 @@ def hash_password(password: str) -> bytes:
     """
     Arguments:
     password (str) ---> user's password
-    
+
     Methods:
     Encode the password to UTF-8 to pass to hashpw
     Generate a salt ---> data added to password before modification
     Pass the password to hashpw with the salt
-    
+
     Returns:
     Salted, hashed password, which is a byte string
     """
