c868dc5072017cebf50c72f390a2ae483cb03fd5
zack6yuan <zack6yuan@gmail.com>
2025-05-21 20:19:12 +0000

    update task.

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Hash Password """
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Arguments:
+    password (str) ---> user's password
+
+    Methods:
+    Encode the password to UTF-8 to pass to hashpw
+    Generate a salt ---> data added to password before modification
+    Pass the password to hashpw with the salt
+
+    Returns:
+    Salted, hashed password, which is a byte string
+    """
+    hashed_pwd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(hashed_pwd, salt)
+    return hashed
