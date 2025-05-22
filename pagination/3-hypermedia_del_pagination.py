e83390d34b89a8f8f875379c35142101d61c4e02
zack6yuan <zack6yuan@gmail.com>
2025-05-22 17:48:01 +0000

    update task 3.

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -42,4 +42,6 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        dictionary = {
+            # add key value pairs here
+        }
