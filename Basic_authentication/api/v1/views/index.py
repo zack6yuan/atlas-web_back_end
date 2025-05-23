af230bd241d41a4bb09b34f33b6cdaf798e27ab2
zack6yuan <zack6yuan@gmail.com>
2025-05-23 22:02:55 +0000

    pycodestyle.

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def handle_abort():
     """ Method: abort 401 """
