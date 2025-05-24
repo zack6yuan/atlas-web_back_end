0c3ccae250d8d08eac1ee83c55ac1ec499593d93
zack6yuan <zack6yuan@gmail.com>
2025-05-24 20:55:55 +0000

    update task.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 
 @app.errorhandler(404)
@@ -31,6 +32,22 @@ def handle_unauthorized(error) -> str:
 def unauthorized_resources(error) -> str:
     """ Handle users accessing unauthorized resources """
     return jsonify({"error": "Forbidden"}), 403
+
+@app.before_request
+def filter_request():
+    if auth is None:
+        return
+    path_list = {
+        ['/api/v1/status/',
+         '/api/v1/unauthorized/',
+         '/api/v1/forbidden/']
+    }
+    if request.path not in path_list:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
