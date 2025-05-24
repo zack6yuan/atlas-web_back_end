c973aabf20cb2ee756191d0c85b752152253ad2b
zack6yuan <zack6yuan@gmail.com>
2025-05-24 19:14:06 +0000

    blank lines?

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,9 +4,10 @@ from flask import request
 from typing import List, TypeVar
 
 
-
 """ Defined Class Auth """
 class Auth:
+    
+    
     """ Initialize Class Auth """
     def __init__(self):
         pass
