e189ed884487d6b8bde3b2bfaf72f6462343a1d1
zack6yuan <zack6yuan@gmail.com>
2025-05-24 19:12:41 +0000

    pycodestyle.

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,16 +4,16 @@ from flask import request
 from typing import List, TypeVar
 
 
+
 """ Defined Class Auth """
 class Auth:
     """ Initialize Class Auth """
     def __init__(self):
         pass
-     
-    def require_auth(self, path:str, excluded_paths: List[str]) -> bool:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth method """
         return False
-
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header method """
