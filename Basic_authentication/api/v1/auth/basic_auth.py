26e6429511ef1c7e0f3d2a7bc9b8226690150be0
zack6yuan <zack6yuan@gmail.com>
2025-05-25 19:21:11 +0000

    update task 7.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,5 +11,12 @@ class BasicAuth(Auth):
         """ Constructor for the class BasicAuth """
         pass
     
-    
-    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        elif not instanceof(authorization_header, str):
+            return None
+        elif authorization_header.startswith("Basic "):
+            return None
+        else:
+            return authorization_header
