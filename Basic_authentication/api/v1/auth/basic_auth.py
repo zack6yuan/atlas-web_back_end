d40e52e023bb15b6aeef0ae40811eef60ad3cd72
zack6yuan <zack6yuan@gmail.com>
2025-05-25 19:36:58 +0000

    update doc.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,6 +24,7 @@ class BasicAuth(Auth):
             return None
         
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract User Credentials Function """
         if decoded_base64_authorization_header is None:
             return None, None
         if not isinstance(decoded_base64_authorization_header, str):
