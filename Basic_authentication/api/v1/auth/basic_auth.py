9bd4243d7530a7fe0fe322b4280ea1f99b5390db
zack6yuan <zack6yuan@gmail.com>
2025-05-25 19:36:22 +0000

    update task.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,3 +15,20 @@ class BasicAuth(Auth):
             return None
         
         return authorization_header
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ Decode Base 64 Authorization Header Function """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        
+        return ("{}:{}".format(str, str))
