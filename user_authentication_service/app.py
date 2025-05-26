64bec69fdd87f5c20b35e0ab1d1c241c95437cfb
zack6yuan <zack6yuan@gmail.com>
2025-05-26 16:28:21 +0000

    update app.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """ Basic Flask App """
 from flask import Flask, jsonify
+from auth import Auth
 
 
 app = Flask(__name__)
+
+
+AUTH = auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +16,18 @@ def main():
         "message": "Bienvenue"
     }
     return jsonify(data)
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users(email, password):
+    payload = {
+        "email": "{}".format(email),
+        "message": "user created"
+    }
+    if not email or not password:
+        return jsonify()
+    
+    
+    
 
 
 if __name__ == "__main__":
