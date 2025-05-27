86c48c5553e690ab461d9933e863970633115865
zack6yuan <zack6yuan@gmail.com>
2025-05-27 04:44:27 +0000

    update.

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """
-The code from the previous task will be put here
+Get Hyper Method
 """
+
+get_page = __import__('1-simple_pagination').get_page
+
+
 def get_hyper(page: int = 1, page_size: int = 10) -> dict:
     """
     Arguments:
     page (int) --> pagination parameter / default value = 1
     page_size (int) --> pagination parameter / default value = 10
     """
+    
