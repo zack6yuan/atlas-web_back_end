ecd86e38d678fb604a4536a3cc52180b1d35b784
zack6yuan <zack6yuan@gmail.com>
2025-05-27 18:25:07 +0000

    update task.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 import re
 from typing import List
+import os
+import mysql.connector
 
-
+"""
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
     Arguments:
     fields (list(str)) --> all fields to obfuscate
     redaction (str) --> what the fiield will be obfuscated by
     message (str) --> log time
     separator(str) --> character separating all log time fields
-    """
+    
     pattern = re.('[eggcellent]', message)
     print(pattern)
+"""
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Connects to holberton databse and returns a connector """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    host = os.getenv("PERSONAL_DATA_DB_HOST")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+    
+    return mysql.connector.connect(username=username,
+                                                            password=password,
+                                                            host=host,
+                                                            database=database
+                                                            )
