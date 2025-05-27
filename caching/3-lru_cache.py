3c75dfbca539b23ad7fb185867df7faa05ab24eb
zack6yuan <zack6yuan@gmail.com>
2025-05-27 04:25:32 +0000

    changes.

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -33,10 +33,8 @@ class LRUCache(BaseCaching):
             self.cache_data[key] = item
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            removed_item, _ = self.cache_data.popitem()
+            removed_item, _ = self.cache_data.popitem(last=False)
             print("DISCARD: {}".format(removed_item))
-
-        self.cache_data[key] = item
 
     def get(self, key):
         """
