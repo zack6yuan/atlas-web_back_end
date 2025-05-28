e8848221406163aceeae5bce0aba5e889a7e20e4
zack6yuan <zack6yuan@gmail.com>
2025-05-28 03:46:53 +0000

    changed the import.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,8 +8,10 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
-from api.v1.auth.auth import require_auth
 import os
+
+
+require_auth = __import__('auth').require_auth
 
 
 app = Flask(__name__)
