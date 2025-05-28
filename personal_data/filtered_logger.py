7edd5b61ea59dca638ca9c485bb4f4345cf42709
zack6yuan <zack6yuan@gmail.com>
2025-05-28 16:50:01 +0000

    update.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ import re
 from typing import List
 import os
 import mysql.connector
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -52,3 +53,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=host,
         database=database
     )
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
