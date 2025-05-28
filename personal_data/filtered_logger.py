4d666dfa24430d9a7dccd8dce5177d3a3a277a8d
zack6yuan <zack6yuan@gmail.com>
2025-05-28 16:36:22 +0000

    well.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,69 +7,9 @@ import mysql.connector
 import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    Arguments:
-    fields (list(str)) --> all fields to obfuscate
-    redaction (str) --> what the fiield will be obfuscated by
-    message (str) --> log time
-    separator(str) --> character separating all log time fields
-    
-    Methods:
-    Regex Expression:
-    (password | date_of_birth) --> matches either
-    [^;]+ --> match characters (one or more) except a semicolon
-    r'\1=' --> ensures the = sign is kept during re.sub with REDACTION
-    
-    Returns:
-    The result of the regex
-    """
-    REDACTION = "xxx"
-    pattern = r'(password|date_of_birth)=([^;]+)'
-    filtered = re.sub(pattern, r'\1=' + REDACTION, message)
-    print(filtered)
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Connects to holberton databse and returns a connector
-    Methods:
-    Get environment variables:
-    --> username
-    --> password
-    --> host
-    --> database
-
-    Returns:
-    An SQL Connector
-    """
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
-    host = os.getenv("PERSONAL_DATA_DB_HOST")
-    database = os.getenv("PERSONAL_DATA_DB_NAME")
-
-    return mysql.connector.connect(
-        username=username,
-        password=password,
-        host=host,
-        database=database
-    )
-    
-def main():
-    """
-    Main Function
-    Args:
-        None
-    Methods:
-        Obtains a database connection and retrieves it
-    Returns:
-        None
-    """
-
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -79,3 +19,63 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
+
+
+    def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+        """
+        Arguments:
+        fields (list(str)) --> all fields to obfuscate
+        redaction (str) --> what the fiield will be obfuscated by
+        message (str) --> log time
+        separator(str) --> character separating all log time fields
+        
+        Methods:
+        Regex Expression:
+        (password | date_of_birth) --> matches either
+        [^;]+ --> match characters (one or more) except a semicolon
+        r'\1=' --> ensures the = sign is kept during re.sub with REDACTION
+        
+        Returns:
+        The result of the regex
+        """
+        pattern = r'(password|date_of_birth)=([^;]+)'
+        filtered = re.sub(pattern, r'\1=' + REDACTION, message)
+        print(filtered)
+
+
+    def get_db() -> mysql.connector.connection.MySQLConnection:
+        """
+        Connects to holberton databse and returns a connector
+        Methods:
+        Get environment variables:
+        --> username
+        --> password
+        --> host
+        --> database
+
+        Returns:
+        An SQL Connector
+        """
+        username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+        password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+        host = os.getenv("PERSONAL_DATA_DB_HOST")
+        database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+        return mysql.connector.connect(
+            username=username,
+            password=password,
+            host=host,
+            database=database
+        )
+        
+    def main():
+        """
+        Main Function
+        Args:
+            None
+        Methods:
+            Obtains a database connection and retrieves it
+        Returns:
+            None
+        """
+
