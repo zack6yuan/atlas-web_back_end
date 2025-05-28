a3e047ba620cd29c545b013ad3a7dd95d0a6860d
zack6yuan <zack6yuan@gmail.com>
2025-05-28 00:03:04 +0000

    update csv.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ import re
 from typing import List
 import os
 import mysql.connector
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -55,4 +56,17 @@ def main():
     Returns:
         None
     """
-    
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
