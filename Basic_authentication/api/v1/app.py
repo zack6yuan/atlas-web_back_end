e2c65b65340ae185d497a2d53a774e6080f58718
zack6yuan <zack6yuan@gmail.com>
2025-05-28 03:45:29 +0000

    update task.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
+from api.v1.auth.auth import require_auth
 import os
 
 
