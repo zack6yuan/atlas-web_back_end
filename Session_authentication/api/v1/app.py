8679f9a2161b90788a1bee6675b8d4701c6f9d8a
zack6yuan <zack6yuan@gmail.com>
2025-05-28 06:11:58 +0000

    changes.

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -6,8 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.auth import Auth
 import os
 
 
