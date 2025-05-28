e8ec097fc0a19bb1c691058c8489512936d15adb
zack6yuan <zack6yuan@gmail.com>
2025-05-28 16:43:52 +0000

    PLEASE.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,78 +4,51 @@ import re
 from typing import List
 import os
 import mysql.connector
-import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-
-    def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-        """
-        Arguments:
-        fields (list(str)) --> all fields to obfuscate
-        redaction (str) --> what the fiield will be obfuscated by
-        message (str) --> log time
-        separator(str) --> character separating all log time fields
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Arguments:
+    fields (list(str)) --> all fields to obfuscate
+    redaction (str) --> what the fiield will be obfuscated by
+    message (str) --> log time
+    separator(str) --> character separating all log time fields
         
-        Methods:
-        Regex Expression:
-        (password | date_of_birth) --> matches either
-        [^;]+ --> match characters (one or more) except a semicolon
-        r'\1=' --> ensures the = sign is kept during re.sub with REDACTION
+    Methods:
+    Regex Expression:
+    (password | date_of_birth) --> matches either
+    [^;]+ --> match characters (one or more) except a semicolon
+    r'\1=' --> ensures the = sign is kept during re.sub with REDACTION
         
-        Returns:
-        The result of the regex
-        """
-        pattern = r'(password|date_of_birth)=([^;]+)'
-        filtered = re.sub(pattern, r'\1=' + REDACTION, message)
-        print(filtered)
+    Returns:
+    The result of the regex
+    """
+    pattern = r'(password|date_of_birth)=([^;]+)'
+    filtered = re.sub(pattern, r'\1=' + redaction, message)
+    print(filtered)
 
 
-    def get_db() -> mysql.connector.connection.MySQLConnection:
-        """
-        Connects to holberton databse and returns a connector
-        Methods:
-        Get environment variables:
-        --> username
-        --> password
-        --> host
-        --> database
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Connects to holberton databse and returns a connector
+    Methods:
+    Get environment variables:
+    --> username
+    --> password
+    --> host
+    --> database
 
-        Returns:
-        An SQL Connector
-        """
-        username = os.getenv("PERSONAL_DATA_DB_USERNAME")
-        password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
-        host = os.getenv("PERSONAL_DATA_DB_HOST")
-        database = os.getenv("PERSONAL_DATA_DB_NAME")
+    Returns:
+    An SQL Connector
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    host = os.getenv("PERSONAL_DATA_DB_HOST")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
 
-        return mysql.connector.connect(
-            username=username,
-            password=password,
-            host=host,
-            database=database
-        )
-        
-    def main():
-        """
-        Main Function
-        Args:
-            None
-        Methods:
-            Obtains a database connection and retrieves it
-        Returns:
-            None
-        """
-
+    return mysql.connector.connect(
+        username=username,
+        password=password,
+        host=host,
+        database=database
+    )
