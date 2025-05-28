de45e3145764f66870e1b562ea71cd0ad19e699c
zack6yuan <zack6yuan@gmail.com>
2025-05-28 19:32:33 +0000

    update.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,8 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.auth import Auth
 import os
 
 
@@ -17,9 +15,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
     
 if getenv("AUTH_TYPE") == 'basic_auth':
+    from api.v1.auth import BasicAuth
     auth = BasicAuth()
     
 if getenv("AUTH_TYPE") == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 @app.errorhandler(404)
@@ -46,7 +46,7 @@ def filter_request():
     if auth is None:
         return
     path_list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path in path_list:
+    if not auth.require_auth(request.path, path_list):
         return
     if auth.authorization_header(request) is None:
         abort(401)
