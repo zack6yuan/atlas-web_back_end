4a7cb13b1ec0229183727b2828726a4bb24478a9
zack6yuan <zack6yuan@gmail.com>
2025-05-28 03:53:44 +0000

    update.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,6 +41,3 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        
