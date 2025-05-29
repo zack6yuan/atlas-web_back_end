275622d2995ad37b23c8293ab802e75959eb6805
zack6yuan <zack6yuan@gmail.com>
2025-05-29 03:42:03 +0000

    update doc.

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -14,11 +14,20 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """ init method """
         self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+        Cached dataset
+        Methods:
+            Checks that the dataset holds no value
+            Opens the CSV file
+            Creates a reader object
+            Creates a new list [index 1 to the end]
+        Returns:
+            The dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -29,7 +38,15 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
+        """
+        Dataset indexed by sorting position, starting at 0
+        Methods:
+            Checks if the dataset has no value assigned to it
+            The result of self.dataset() is stored in "dataset"
+            Slices the list
+            Dictionary comprehension
+        Returns:
+            The dictionary (indexed_dataset)
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -40,7 +57,21 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """ Get Hyper Index Function """
+        """
+        Get Hyper Index Function
+        Arguments:
+            index --> the starting point for the page
+            page_size --> the number of items on the page
+        Methods:
+            Check if page_size is an integer (assert)
+            Check is page_size is > 0 (assert)
+            Retrieve the dataset and assign to new_dataset
+            Add two to the index provided (next_index)
+        Errors:
+            AssertionError raised it index >- len(new_dataset)
+        Returns:
+            A dictionary with key-value pairs according to format
+        """
         assert isinstance(page_size, int)
         assert page_size > 0
 
