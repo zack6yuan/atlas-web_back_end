eb4007c0b3218c29067a63fb17e6c70692a03fbf
zack6yuan <zack6yuan@gmail.com>
2025-05-29 03:28:24 +0000

    changes.

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(
-        self, index: int = None,
+        self,
+        index: int = None,
         page_size: int = 10
     ) -> Dict:
         assert isinstance(page_size, int)
