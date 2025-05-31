bbdcbf65c5f0187b92acb8ce3c4f96a208766306
zack6yuan <zack6yuan@gmail.com>
2025-05-31 19:48:09 +0000

    update imports and add type hints.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,13 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -11,10 +18,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b",), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected_result):
         test_result = access_nested_map(nested_map, path)
         self.assertEqual(test_result, expected_result)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
