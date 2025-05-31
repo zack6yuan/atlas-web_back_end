bb12a050320e6b0ac5a186df7d69db595f3bc460
zack6yuan <zack6yuan@gmail.com>
2025-05-31 22:27:34 +0000

    update task.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """ Format: Argument, Path, Output """
     @parameterized.expand([
         param({"a": 1}, ("a",), 1),
         param({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -15,6 +16,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test Access Nested Map Function Test """
         test_result = access_nested_map(nested_map, path)
         self.assertEqual(test_result, expected_result)
+    
+    @parameterized.expand([
+        param({}, ("a",), {}),
+        param({"a": 1}, ("a", "b",), 1),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_result):
+        with self.assertRaises(KeyError):
+            test_result = access_nested_map(nested_map, path)
+            raise KeyError("Failed: KeyError")
 
 
 if __name__ == '__main__':
