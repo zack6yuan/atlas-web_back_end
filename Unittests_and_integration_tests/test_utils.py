ea7658a4040b1c404fb0dcdce7d2483be3d07abc
zack6yuan <zack6yuan@gmail.com>
2025-05-31 22:19:17 +0000

    import param?

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """ Test Utils Unittest Module """
 import unittest
-from parameterized import parameterized
+from parameterized import parameterized, param
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b",), 2),
+        param({"a": 1}, ("a",), 1),
+        param({"a": {"b": 2}}, ("a",), {"b": 2}),
+        param({"a": {"b": 2}}, ("a", "b",), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """ Test Access Nested Map Function Test """
