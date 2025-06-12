8a494d63005ba5a10e9a317ecc892725d634aa29
zack6yuan <zack6yuan@gmail.com>
2025-06-12 14:44:57 -0500

    update task 1.

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table "users" with required attributes
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+)
