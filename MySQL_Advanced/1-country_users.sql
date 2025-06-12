5453c1fdda9701e40946cb7c297b25b1f5903a30
zack6yuan <zack6yuan@gmail.com>
2025-06-12 14:46:08 -0500

    id should be int.

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- Creates a table "users" with required attributes
 CREATE TABLE IF NOT EXISTS users (
-    id VARCHAR NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL
