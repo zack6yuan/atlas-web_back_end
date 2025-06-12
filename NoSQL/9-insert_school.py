ce24ac2d6c8a9a960da83259176c1f035b9e8db5
zack6yuan <zack6yuan@gmail.com>
2025-06-12 10:28:26 -0500

    updates.

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def insert_school(mongo_collection, **kwargs):
+    """
+    Params:
+        mongo_collection: mongodb collection
+        **kwargs: multiple arguments
+
+    Returns:
+        the new _id
+    """
