951594a2ebfe32ceaf4cde5b7635bd00d235d9e7
zack6yuan <zack6yuan@gmail.com>
2025-06-14 03:55:07 +0000

    update task.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
-from typing import String
+from typing import String, Union
+import uuid
 
 
 class Cache:
+    """ Cache Class """
     def __init__(self):
+        """ init method """
         pass
 
-    def store(self, data) -> str:
-        pass
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Params:
+            self --> instance of the class
+            data --> data argument
+        Methods:
+            generates a random key --> uuid
+            stores input data in Redis with random key
+        Returns:
+            the key
+        """
+        new_key = uuid.uuid4()
+        input_data = redis.Redis()
+        
