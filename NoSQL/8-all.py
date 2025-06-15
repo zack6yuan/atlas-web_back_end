d20dd2ca1fa199fe441d3825e03ec2118bc3d659
zack6yuan <zack6yuan@gmail.com>
2025-06-15 18:11:56 +0000

    update doc.

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+""" List all documents in Python """
 from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    """ Lists all databases in a collection """
+    """ Lists all documents in a collection """
     documents = mongo_collection.find({})
     for document in documents:
         return(document)
