1ab82ee2dc46f60067a208fb3b8d473763f63b48
zack6yuan <zack6yuan@gmail.com>
2025-06-17 03:22:38 +0000

    update task.

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,5 +1,5 @@
 -- Creates a function SafeDiv
-
+DELIMITER $ -- set delimiter so function does not end with semicolons
 CREATE FUNCTION SafeDiv(
     a INT,
     b INT
@@ -7,7 +7,10 @@ CREATE FUNCTION SafeDiv(
 RETURNS FLOAT
 BEGIN
     IF b = 0 THEN
-        RETURN 0
+        RETURN 0;
     ELSE
-        RETURN a / b
-END
+        RETURN a / b;
+    END IF;
+END $$
+
+DELIMITER ; -- set the delimiter back to the semicolon
