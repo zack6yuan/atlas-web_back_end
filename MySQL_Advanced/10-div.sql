956ac613b729cf1d6d92fc59d9cfa2300124b03e
zack6yuan <zack6yuan@gmail.com>
2025-06-17 13:48:31 -0500

    upadte.

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,10 +1,10 @@
 -- Creates a function SafeDiv
-DELIMITER // -- set delimiter so function does not end with semicolons
+DELIMITER //
 CREATE FUNCTION SafeDiv(
     a INT,
     b INT
 )
-RETURNS FLOAT
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
@@ -13,4 +13,4 @@ BEGIN
     END IF;
 END //
 
-DELIMITER ; -- set the delimiter back to the semicolon
+DELIMITER ;
