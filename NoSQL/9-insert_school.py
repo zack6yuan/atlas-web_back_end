28fe4c00b823ff03bd228d766b8fb36f0a66f11e
zack6yuan <zack6yuan@gmail.com>
2025-06-17 00:02:03 -0500

    update task 9.

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Insert a document in Python """
-from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs """
-    
+    document = mongo_collection.insert_one(kwargs)
+    return document.inserted_id
