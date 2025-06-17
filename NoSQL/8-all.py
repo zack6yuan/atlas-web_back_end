2fdc34137f9386d68a10b3d125df0682112af326
zack6yuan <zack6yuan@gmail.com>
2025-06-16 23:58:21 -0500

    update task 8.

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ List all documents in Python """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """ Lists all documents in a collection """
     document_list = []
-    documents = mongo_collection.find({})
+    documents = mongo_collection.find()
     for document in documents:
         document_list.append(document)
     return document_list
