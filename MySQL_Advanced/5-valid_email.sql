c88d7516dd53dc175c18e8507f8d2cea7988d9d2
zack6yuan <zack6yuan@gmail.com>
2025-06-17 12:23:03 -0500

    upadte syntax, and I spelled delimiter wrong again.

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,9 @@
 -- Creates a trigger that resets the attribute valid_email
 -- Only when the email has been changed
-DELIMETER //
+DELIMITER //
 CREATE TRIGGER reset_email BEFORE INSERT ON email
 FOR EACH ROW
 BEGIN
-    SET valid_email = NULL
+    SET NEW.valid_email = NULL;
 END;//
-DELIMETER ;
+DELIMITER ;
