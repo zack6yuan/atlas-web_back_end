70fca13b2eb0bd8cc13399002b25a50cb86d726d
zack6yuan <zack6yuan@gmail.com>
2025-06-17 02:57:58 +0000

    update task 3.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- Lists all bands with "Glam rock" as their main style, ranked by their longevity
+-- Coalesce - if not null, return the split year, if not, band is still current
 SELECT
-    band_name, split - formed AS lifespan
+    band_name, IFNULL(split, 2025) - formed AS lifespan
 FROM
     metal_bands
 WHERE
