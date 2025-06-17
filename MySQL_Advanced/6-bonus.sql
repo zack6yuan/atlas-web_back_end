af8652fc9af207b38c957c093fc80aad46e3f997
zack6yuan <zack6yuan@gmail.com>
2025-06-17 13:34:04 -0500

    update task.

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,10 +1,13 @@
 -- Creates a stored procedure AddBonus that adds a new correction for a student
+DELIMITER //
 CREATE PROCEDURE AddBonus(
     IN user_id INT,
     IN project_name VARCHAR(100),
     IN score INT
 )
 BEGIN
-INSERT INTO corrections(
-    
-)
+    INSERT INTO corrections(
+        user_id, project_id, score
+    );
+END;
+DELIMITER ;  
--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,1 +1,10 @@
 -- Creates a stored procesure that computes and stores the average score for a student
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    SELECT AVG(score) AS average_score
+    FROM corrections;
+END //
+DELIMITER ;
