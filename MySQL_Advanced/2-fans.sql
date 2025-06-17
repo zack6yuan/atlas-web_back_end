45697906c972aa5ec353e9c2c1e5a99adbd6dd7d
zack6yuan <zack6yuan@gmail.com>
2025-06-17 02:37:53 +0000

    update task 2.

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,4 +1,9 @@
 -- Ranks country origin of bands ordered by the number of fans (non-unique)
-SELECT origin, nb_fans
-FROM metal_bands
-ORDER BY nb_fans DESC
+SELECT
+    origin, SUM(fans) AS nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC
