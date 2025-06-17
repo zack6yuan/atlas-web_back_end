72a9a1d8c778795e9352bdd0e15f771258b641b0
zack6yuan <zack6yuan@gmail.com>
2025-06-17 13:43:07 -0500

    update.

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -4,8 +4,13 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    INSERT INTO score
-    SELECT AVG(score) AS average_score
-    FROM corrections;
+    INSERT INTO
+        score
+    SELECT
+        AVG(score)
+    FROM
+        corrections
+    WHERE
+        user_id = user_id;
 END //
 DELIMITER ;
