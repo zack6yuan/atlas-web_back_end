cd3f05f0fe7147228bde89cd9a7a3d6023f1a160
zack6yuan <zack6yuan@gmail.com>
2025-06-17 12:24:41 -0500

    insert to update.

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- Creates a trigger that resets the attribute valid_email
 -- Only when the email has been changed
 DELIMITER //
-CREATE TRIGGER reset_email BEFORE INSERT ON email
+CREATE TRIGGER reset_email BEFORE UPDATE ON email
 FOR EACH ROW
 BEGIN
     SET NEW.valid_email = NULL;
