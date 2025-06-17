783c1bcba2668298c9fd64313d9c61ed4fb863b0
zack6yuan <zack6yuan@gmail.com>
2025-06-17 12:20:16 -0500

    upadte task 5.

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,2 +1,9 @@
 -- Creates a trigger that resets the attribute valid_email
 -- Only when the email has been changed
+DELIMETER //
+CREATE TRIGGER reset_email AFTER INSERT ON email
+FOR EACH ROW
+BEGIN
+    SET valid_email = NULL
+END;//
+DELIMETER ;
