aabe7e31b321672130aeeb6ae3426c684c315e58
zack6yuan <zack6yuan@gmail.com>
2025-06-23 19:51:55 +0000

    updated task 0.

--- Node_JS/0-console.js
+++ Node_JS/0-console.js
@@ -1,1 +1,7 @@
+#!/usr/bin/env node
 // function displayMessage that prints in STDOUT the string argument
+function displayMessage(arg) {
+    console.log(arg);
+}
+
+module.exports = displayMessage;
