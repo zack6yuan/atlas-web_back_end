92f3717f5837f5704c5e7e8e9a8fd36ef1dc9325
zack6yuan <zack6yuan@gmail.com>
2025-06-26 03:36:58 +0000

    update task 1.

--- Node_JS/1-stdin.js
+++ Node_JS/1-stdin.js
@@ -1,1 +1,14 @@
+#!/usr/bin/env node
 // program 1-stdin.js that is executed through the command line
+const readline = require("readline")
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+input.question(`Welcome to Holberton School, what is your name? \n`, name => {
+    console.log(`Your name is: ${name}`);
+    console.log("This important software is now closing \n");
+    input.close()
+})
