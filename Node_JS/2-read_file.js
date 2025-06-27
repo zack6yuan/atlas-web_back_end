3eea589cc688d73b817ced47d180586cabdf173f
zack6yuan <zack6yuan@gmail.com>
2025-06-27 20:18:47 +0000

    update.

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 // function countStudents using database.csv
-function countStudents(path) {
-    
+function countStudents() {
+    const fs = require('fs');
+    const data = fs.readFileSync('./database.csv')
+    if (!(data)) {
+        console.log("Cannot load the database")
+    }
 }
+
+
+module.exports = countStudents()
