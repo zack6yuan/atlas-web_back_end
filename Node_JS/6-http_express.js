3aa0e82bf9269474bfc6394f3186b7954eb3d173
zack6yuan <zack6yuan@gmail.com>
2025-06-27 20:58:57 +0000

    update.

--- Node_JS/6-http_express.js
+++ Node_JS/6-http_express.js
@@ -1,1 +1,9 @@
+#!/usr/bin/env node
 // Small HTTP server using the Express module
+const express = require('express')
+const app = express()
+const port = 1245
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
