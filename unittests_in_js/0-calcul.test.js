7efe6cfaddd17a018243fed710a2b2a3c8efde61
zack6yuan <zack6yuan@gmail.com>
2025-06-27 21:11:44 +0000

    update.

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,5 +3,8 @@ const calculateNumber = require('./0-calcul.js')
 describe('CalculateNumber', function() {
     it('should return the sum of integers', function() {
         assert.equal(calculateNumber(1, 3), 4);
+        assert.equal(calculateNumber(1, 3.7), 5);
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+        assert.equal(calculateNumber(1.5, 3.7), 6);
     });
 });
