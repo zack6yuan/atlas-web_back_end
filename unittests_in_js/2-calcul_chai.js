a4e70c58674946da99cd477a1c8b9b11d728e3cd
zack6yuan <zack6yuan@gmail.com>
2025-06-29 05:54:08 +0000

    used expect.

---
+++ unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+// Calculate number function
+function calculateNumber(type, a, b) {
+    let new_a = Math.round(a)
+    let new_b = Math.round(b)
+    if (type === "SUM") {
+        return new_a + new_b;
+    } else if (type === "SUBTRACT") {
+        return new_a - new_b;
+    } else if (type === "DIVIDE") {
+        if (new_b === 0) {
+            return "Error"
+        }
+        return new_a / new_b;
+    }
+}
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const calculateNumber = require('./1-calcul.js')
+describe('CalculateNumber', function () {
+    it('should return the sum, difference, and quotient of integers', function () {
+        expect(calculateNumber("SUM", 1.4, 1.5)).toEqual(6)
+        expect(calculateNumber("SUBTRACT", 1.4, 4.5)).toEqual(-4)
+        expect(calculateNumber("DIVIDE", 1.4, 4.5)).toEqual(0.2)
+        expect(calculateNumber("DIVIDE", 1.4, 0)).toEqual("Error")
+    });
+});
