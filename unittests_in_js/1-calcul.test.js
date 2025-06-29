6ec35e85fef0f6ebb70870435ca2c38cf3b6bd15
zack6yuan <zack6yuan@gmail.com>
2025-06-29 05:31:02 +0000

    update.

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -5,6 +5,6 @@ describe('CalculateNumber', function() {
         assert.equal(calculateNumber("SUM", (1.4, 4.5), 6));
         assert.equal(calculateNumber("SUBTRACT", (1.4, 4.5), -4));
         assert.equal(calculateNumber(("DIVIDE", 1.4, 4.5), 0.2));
-        assert.equal(calculateNumber(("DIVIDE", 1.4, 0), "'Error'"));
+        assert.equal(calculateNumber(("DIVIDE", 1.4, 0), "Error"));
     });
 });
