1dfe009bb13887f7c68734c34f9d315d76a1c2a3
zack6yuan <zack6yuan@gmail.com>
2025-06-29 07:09:24 +0000

    update.

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const chai = require('chai')
+const {expect} = chai;
 const calculateNumber = require('./1-calcul.js')
 describe('CalculateNumber', function () {
     it('should return the sum, difference, and quotient of integers', function () {
