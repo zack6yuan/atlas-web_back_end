567b93e8a11e447c57a2135220eb9d5b326cff28
zack6yuan <zack6yuan@gmail.com>
2025-06-29 07:10:37 +0000

    update syntax.

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -4,9 +4,9 @@ const {expect} = chai;
 const calculateNumber = require('./1-calcul.js')
 describe('CalculateNumber', function () {
     it('should return the sum, difference, and quotient of integers', function () {
-        expect(calculateNumber("SUM", 1.4, 1.5)).toEqual(6)
-        expect(calculateNumber("SUBTRACT", 1.4, 4.5)).toEqual(-4)
-        expect(calculateNumber("DIVIDE", 1.4, 4.5)).toEqual(0.2)
-        expect(calculateNumber("DIVIDE", 1.4, 0)).toEqual("Error")
+        expect(calculateNumber("SUM", 1.4, 1.5)).to.equal(6)
+        expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4)
+        expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2)
+        expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error")
     });
 });
