36cacfc6cdb58b44932dce1d4b2da2aaf8754798
zack6yuan <zack6yuan@gmail.com>
2025-06-30 03:59:48 +0000

    update task 1.

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+const client = createClient();
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+client.on('success', success => console.log("Redis client connected to the server"));
+await client.connect();
+
+function setNewSchool(schoolName, value) {
+    redis.set(schoolName, value);
+    redis.print("Values set!");
+}
+
+function displaySchoolValue(schoolName) {
+    value = redis.get(schoolName);
+    console.log(value);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
