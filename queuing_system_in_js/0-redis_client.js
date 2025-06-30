4c359a138394d095889c8d5d59750c4aaf6233f5
zack6yuan <zack6yuan@gmail.com>
2025-06-30 04:03:35 +0000

    logic.

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -1,5 +1,5 @@
 import { createClient } from 'redis';
 const client = createClient();
 client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
-client.on('success', success => console.log("Redis client connected to the server"));
 await client.connect();
+console.log("Redis client connected to the server");
