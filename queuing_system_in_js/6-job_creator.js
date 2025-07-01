49db77dc499df601e7f8b7e90293763e341346a3
zack6yuan <zack6yuan@gmail.com>
2025-07-01 17:32:24 -0500

    added doc.

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -6,14 +6,17 @@ queue.create('push_notification_code', {
     message: String
 }).save()
 
+// Job created experiences error
 queue.on('error', function() {
     console.log('Notification job failed')
 })
 
+// Job is successfully creted
 queue.on('job enqueue', function(id) {
     console.log(`Notification job created: ${id}`)
 })
 
+// Job is completed
 queue.on('job complete', function() {
     console.log('Notification job completed')
 })
