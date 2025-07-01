0cc45a002d9b7ed787de03dec2a5cabb70fb6983
zack6yuan <zack6yuan@gmail.com>
2025-07-01 18:54:47 -0500

    update.

---
+++ queuing_system_in_js/9-server_get.js
@@ -0,0 +1,34 @@
+const listProducts = [
+    {
+        id: 1,
+        name: "Suitcase 250",
+        price: 50,
+        stock: 4
+    },
+    {
+        id: 2,
+        name: "Suitcase 450",
+        price: 100,
+        stock: 10
+    },
+    {
+        id: 3,
+        name: "Suitcase 650",
+        price: 350,
+        stock: 2
+    },
+    {
+        id: 4,
+        name: "Suitcase 1050",
+        price: 500,
+        stock: 5
+    }
+]
+
+function getItemById(id) {
+    for (item of listProducts) {
+        if (search == id) {
+            return 
+        }
+    }
+}
