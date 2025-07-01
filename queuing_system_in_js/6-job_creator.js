de16acccd7e15fa0fdd66ed3882fb90bd4d42b2a
zack6yuan <zack6yuan@gmail.com>
2025-07-01 17:28:04 -0500

    update task 6.

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -1,2 +1,19 @@
-const redis = require('redis')
-queue.on('new_job', function(phoneNumber, string))
+const kue = require('kue')
+const queue = kue.createQueue()
+
+queue.create('push_notification_code', {
+    phoneNumber: String,
+    message: String
+}).save()
+
+queue.on('error', function() {
+    console.log('Notification job failed')
+})
+
+queue.on('job complete', function() {
+    console.log('Notification job completed')
+})
+
+queue.on('job complete', function(id) {
+    console.log(`Notification job created: ${id}`)
+})
