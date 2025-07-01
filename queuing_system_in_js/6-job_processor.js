503fc1884c12e42703d540b31a96b6aaacf03e87
zack6yuan <zack6yuan@gmail.com>
2025-07-01 18:08:09 -0500

    update task 6.

--- queuing_system_in_js/6-job_processor.js
+++ queuing_system_in_js/6-job_processor.js
@@ -1,9 +1,20 @@
 const kue = require('kue')
-const queue = require('queue')
+const queue = kue.createQueue()
 
 queue.create('push_notification_code', {
     phoneNumber: String,
     message: String
 }).save()
 
-queue
+function sendNotification(phoneNumber, message) {
+    console.log(`Sending notification to ${phoneNumber}, with message ${message}`)
+}
+
+queue.create('push_notification_code', {
+    phoneNumber: String,
+    message: String
+}).save()
+
+queue.on('job enqueue', function(phoneNumber, message) {
+    sendNotification(phoneNumber, message)
+})
