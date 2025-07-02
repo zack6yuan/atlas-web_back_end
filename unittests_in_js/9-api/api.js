e4cab411fa8613cf2fcbf52421e54caf3c3f80c8
zack6yuan <zack6yuan@gmail.com>
2025-07-02 12:16:50 -0500

    update.

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+port = 7865
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system')
+})
+
+app.get('/cart/:id', (req, res) => {
+    console.log(`Payment methods for cart: ${id}`)
+})
+
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`)
+})
