482cc7645dcaad2e2f3af2bebf93015f02e391c0
zack6yuan <zack6yuan@gmail.com>
2025-07-02 11:10:39 -0500

    seperate test suites.

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,10 +1,16 @@
 const assert = require('assert')
 const calculateNumber = require('./0-calcul.js')
 describe('CalculateNumber', function() {
-    it('should return the sum of integer arguments', function() {
+    it('should return the sum of two arguments', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+    it('should return two added arguments and round up', function() {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+    it('should return the sum of two arguments', function() {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+    it ('should return two added arguments and round down', function() {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    })
 });
