8e4c5d19f625548d94f76e591e4a32ee3df83343
zack6yuan <zack6yuan@gmail.com>
2025-07-02 11:02:33 -0500

    strictEqual?

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,9 +2,9 @@ const assert = require('assert')
 const calculateNumber = require('./0-calcul.js')
 describe('CalculateNumber', function() {
     it('should return the sum of integers', function() {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1, 3.7), 5);
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
