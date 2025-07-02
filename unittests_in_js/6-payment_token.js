d3e0cd0f9951abb84057d68a199f987702c2dbd9
zack6yuan <zack6yuan@gmail.com>
2025-07-02 11:50:45 -0500

    update task.

---
+++ unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+function getPaymentTokenFromAPI(success) {
+    if (success == "true") {
+        return Promise.resolve({
+            data: 'Successful response from the API'
+        })
+    } else {
+        return
+    }
+}
---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai')
+const { expect } = chai;
+const { getPaymentTokenFromAPI } = require('./6-payment_token');
+describe('PaymentToken', function() {
+    it('should ensure the result is true', function() {
+        expect(getPaymentTokenFromAPI).to.be.true
+    });
+});
