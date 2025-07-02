662ac882b176a8df7a0b751cbe287eeb2bd92f66
zack6yuan <zack6yuan@gmail.com>
2025-07-02 18:26:59 +0000

    update.

--- Node_JS/7-http_express.js
+++ Node_JS/7-http_express.js
@@ -1,1 +1,12 @@
-// Recreating the small HTTP server using express
+#!/usr/bin/env node
+const express = require('express')
+const app = express()
+const port = 1245
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!')
+})
+
+app.get('/students', (req, res) => {
+    res.send("This is the list of our students")
+})
