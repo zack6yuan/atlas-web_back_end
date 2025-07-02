1e93f463d7e1c168dfa4d0a8f0e8c7f2acae965d
zack6yuan <zack6yuan@gmail.com>
2025-07-02 11:03:17 -0500

    update.

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const calculateNumber = require('./0-calcul.js')
 describe('CalculateNumber', function() {
-    it('should return the sum of integers', function() {
+    it('should return the sum of integer arguments', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
