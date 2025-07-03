4bd6e6efc801d87d973615b335f275be8b18c042
zack6yuan <zack6yuan@gmail.com>
2025-07-03 19:37:39 +0000

    update doc.

--- queuing_system_in_js/5-subscriber.js
+++ queuing_system_in_js/5-subscriber.js
@@ -1,22 +1,25 @@
-const redis = require('redis')
+const redis = require('redis') // use the redis module
 
 try {
-    await client.connect()
-    console.log("Redis client connected to the server")
-} catch (err) {
-    (`Redis client not connected to the server: ${err}`)
+    await client.connect() // try to connect
+    console.log("Redis client connected to the server") // log success messsage to the console
+} catch (err) { // if error
+    (`Redis client not connected to the server: ${err}`) // log error message to the console
 }
 
 (async () => {
-    const client = redis.createClient()
-    const subscriber = client.duplicate()
-    await subscriber.connect()
+    /*
+    ASYNC FUNCTION
+    */
+    const client = redis.createClient() // crete a Redis client
+    const subscriber = client.duplicate() // create a separate connection
+    await subscriber.connect() // try to connect
     await subscriber.subscribe('holberton school channel', (message) => {
-        if (message === "KILL_SERVER") {
-            subscriber.unsubscribe()
-            subscriber.quit()
+        if (message === "KILL_SERVER") { // if the message is "KILL SERVER"
+            subscriber.unsubscribe() // unsubscribe
+            subscriber.quit() // quit
         } else {
-            console.log(message)
+            console.log(message) // log messag to the console
         }
     })
 })
