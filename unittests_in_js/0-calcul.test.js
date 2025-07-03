2418cd5493e4c659f78369766ea91d82a151504d
zack6yuan <zack6yuan@gmail.com>
2025-07-03 12:39:08 -0500

    update.

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -11,13 +11,13 @@ describe('CalculateNumber', function() {
     it('should return the sum of two arguments', function() {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
-    it ('should return two added arguments and round down', function() {
+    it('should return two added arguments and round down', function() {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
-    it ('should round both numbers down and get the sum', function() {
+    it('should round both numbers down and get the sum', function() {
         assert.strictEqual(calculateNumber(1.4, 2.2), 3);
     });
-    it ('should round both numbers up and get the sum', function() {
+    it('should round both numbers up and get the sum', function() {
         assert.strictEqual(calculateNumber(1.6, 2.8), 5);
     });
 });
