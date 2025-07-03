55700c5fcabb0afa9e17a8f88d048091c71c8bf1
zack6yuan <zack6yuan@gmail.com>
2025-07-03 19:17:44 +0000

    update doc.

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -1,9 +1,9 @@
-const redis = require('redis')
-const client = redis.createClient()
+const redis = require('redis') // use the redis module
+const client = redis.createClient() // create a redis client
 
 try {
-    await client.connect()
-    console.log("Redis client connected to the server")
-} catch (err) {
-    console.log(`Redis client not connected to the server ${err}`)
+    await client.connect() // wait for the connection
+    console.log("Redis client connected to the server") // connection message
+} catch (err) { // if error
+    console.log(`Redis client not connected to the server ${err}`) // log to console with error mesage
 }
