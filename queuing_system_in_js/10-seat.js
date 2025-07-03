b2fda2839aa164bc4327ea8ec460d30b552c1e08
zack6yuan <zack6yuan@gmail.com>
2025-07-03 19:59:08 +0000

    upadte doc.

--- queuing_system_in_js/10-seat.js
+++ queuing_system_in_js/10-seat.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 1245
+const express = require('express') // require the express module
+const app = express() // assign to the variable app
+const port = 1245 // set the app to run on port 1245
 
-let available_seats = 50
+let available_seats = 50 // initialize abailable seats
 
-const kue = require('kue')
-const queue = kue.createQueue()
+const kue = require('kue') // require the Kue module
+const queue = kue.createQueue() // create a queue with Kue
 
-queue.create('reserve_seat', {
+queue.create('reserve_seat', { // create queue "reserve_seat"
 }).save()
 
 function reserveSeat(number) {
     const data = {
         available_seats: number
     };
-    return (data);
+    return (data); // return the data
 }
 
 function getCurrentAvailableSeats() {
-    return (available_seats);
+    return (available_seats); // retuen the available seats
 }
