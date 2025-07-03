7b67aeda04e4e5bef7036270ebc7713e1804f0b0
zack6yuan <zack6yuan@gmail.com>
2025-07-03 19:21:39 +0000

    update doc.

--- queuing_system_in_js/1-redis_op.js
+++ queuing_system_in_js/1-redis_op.js
@@ -1,22 +1,43 @@
-const redis = require('redis')
-const client = redis.createClient()
+const redis = require('redis') // use the redis module
+const client = redis.createClient() // create a redis client
 
 try {
-    await client.connect()
-    console.log("Redis client connected to the server")
-} catch (err) {
-    console.log(`Redis client not connected to the server ${err}`)
+    await client.connect() // try to connect
+    console.log("Redis client connected to the server") // log message to the console
+} catch (err) { // if error
+    console.log(`Redis client not connected to the server ${err}`) // log error message to console
 }
 
 function setNewSchool(schoolName, value) {
+    /*
+    params -->
+    schoolName - name of the school
+    value - value of the school
+
+    methods -->
+    sets in Redis the value for the key schoolName
+
+    returns -->
+    nothing
+    */
     redis.set(schoolName, value, redis.print);
 }
 
 function displaySchoolValue(schoolName) {
+    /*
+    params -->
+    schoolName - name of the school
+
+    methods -->
+    get the value of the school name
+
+    returns -->
+    logs to the console the value
+    */
     value = redis.get(schoolName);
     console.log(value);
 }
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+displaySchoolValue('Holberton'); // call displaySchoolValue with arg
+setNewSchool('HolbertonSanFrancisco', '100'); // call setNewSchool with arg
+displaySchoolValue('HolbertonSanFrancisco'); // call displaySchoolValue with arg
