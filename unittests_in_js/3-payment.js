6064d25188d1ed8538221b68dac45c9f8a22f31d
zack6yuan <zack6yuan@gmail.com>
2025-07-03 11:06:46 -0500

    semicolons.

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
-const { Utils } = require('./utils.js')
+const { Utils } = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping)
-    console.log(`The total is: ${result}`)
+    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
 }
 
-module.exports = sendPaymentRequestToApi
+module.exports = sendPaymentRequestToApi;
