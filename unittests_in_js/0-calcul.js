15d874462e0af4ec1cc67b8ec93a3096cdc0f3a9
zack6yuan <zack6yuan@gmail.com>
2025-07-03 12:34:02 -0500

    revert.

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 // Calculate number function
 function calculateNumber(a, b){
-    a_round = Math.round(a)
-    b_round = Math.round(b)
-    sum = a_round + b_round
-    
-    return sum
+    return Math.round(a) + Math.round(b)
 }
 
 module.exports = calculateNumber;
