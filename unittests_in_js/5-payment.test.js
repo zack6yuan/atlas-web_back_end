78da366718ba1d07a33c938559daec12ea5387c7
zack6yuan <zack6yuan@gmail.com>
2025-07-03 11:55:56 -0500

    update.

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,12 @@
+const chai = require('chai')
+const { expect } = chai;
+describe('sendPaymentRequestToApi', function() {
+    it('should verify the console is logging', function() {
+        expect(console.log.calledWith('The total is: 120')).to.be.true;
+        expect(console.log.calledOnce).to.be.true;
+    })
+    it('should verify the console is logging', function() {
+        expect(console.log.calledWith('The total is: 20')).to.be.true;
+        expect(console.log.calledOnce).to.be.true;
+    })
+})
