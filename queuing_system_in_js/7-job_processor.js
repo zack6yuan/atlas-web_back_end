f2ea32a6fd283e9ceadada551c46b115e5b53915
zack6yuan <zack6yuan@gmail.com>
2025-07-03 19:44:51 +0000

    update doc.

--- queuing_system_in_js/7-job_processor.js
+++ queuing_system_in_js/7-job_processor.js
@@ -1,6 +1,7 @@
-const kue = require('kue')
-const queue = kue.createQueue()
+const kue = require('kue') // require the Kue module
+const queue = kue.createQueue() // create a queue with Kue
 
+// phone numbers to be blacklisted
 const blacklist = [
     '4153518780',
     '4153518781'
@@ -8,13 +9,13 @@ const blacklist = [
 
 function sendNotification(phoneNumber, message, job, done) {
     // sendNotification function
-    if (blacklist.includes(phoneNumber)) {
-        start_progress = job.progress(0, 100);
-        queue.on('error', function(err) {
-            console.log(`Phone number ${phoneNumber} is blacklisted`);
+    if (blacklist.includes(phoneNumber)) { // if the phone number is in the blacklist
+        start_progress = job.progress(0, 100); // track the job progress 0 out of 100
+        queue.on('error', function(err) { // on err
+            console.log(`Phone number ${phoneNumber} is blacklisted`); // log to the console the phone number
         })
     } else {
-        new_progress = job.progress(50, 100);
-        console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+        new_progress = job.progress(50, 100); // track the job progress 50 out of 100
+        console.log(`Sending notification to ${phoneNumber}, with message: ${message}`); // log message to the console
     };
 };
