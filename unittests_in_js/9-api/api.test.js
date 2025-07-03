a3c3fd7b35bf8781dc2f5f03e2be1d352fb06ccd
zack6yuan <zack6yuan@gmail.com>
2025-07-03 00:16:37 -0500

    update.

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
 const app = require('api,js')
-describe('api', function() {
-    it('should give the status code 200 for success', )
+describe('api', () => {
+    assert.equal(res.statusCode, 200)
 })
