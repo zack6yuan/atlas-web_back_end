0c8b97b52bd104d08da7014cd906c2a898d11226
zack6yuan <zack6yuan@gmail.com>
2025-07-03 19:58:04 +0000

    update.

--- queuing_system_in_js/10-in_stock.js
+++ queuing_system_in_js/10-in_stock.js
@@ -1,6 +1,6 @@
 // This is the module for task 16, In stock?
-const express = require('express')
-const app = express()
+const express = require('express') // require the express module
+const app = express() // assign to the variable "app"
 
 const listProducts = [
     {
@@ -30,9 +30,9 @@ const listProducts = [
 ]
 
 function getItemById(id) {
-    for (item of listProducts) {
-        if (item.id === id) {
-            return item
+    for (item of listProducts) { // get the item in array listProducts
+        if (item.id === id) { // if the id matches
+            return item // reutrn the item
         }
     }
 }
@@ -41,22 +41,24 @@ app.get('/list_products', (req, res) => {
     res.send(listProducts)
 })
 
-const redis = require('redis')
-const client = redis.createClient()
+const redis = require('redis') // require the redis module
+const client = redis.createClient() // create a redis client
 
 try {
-    await client.connect()
-    console.log("Redis client connected to the server")
-} catch (err) {
-    console.log(`Redis client not connected to the server ${err}`)
+    await client.connect() // try to connect
+    console.log("Redis client connected to the server") // log success message to the console
+} catch (err) { // if error
+    console.log(`Redis client not connected to the server ${err}`) // log the error message to the console
 }
 
 function reserveStockById(itemId, stock) {
     // Set in Redis the stock for the key item.Item_ID
+    client.set(itemId, stock)
 }
 
 async function getCurrentReservedStockById(itemID) {
     // Return the reserved stock for a specific item
+    console.log(itemID)
 }
 
 app.get('/list_products/:itemId', (req, res) => {
@@ -64,5 +66,13 @@ app.get('/list_products/:itemId', (req, res) => {
 })
 
 app.get('/reserve_product/:itemId', (req, res) => {
-    
+    if (listProducts.includes(item)) {
+        if (stock > 1) {
+            console.log({"status":"Reservation confirmed","itemId":1})
+        } else if (stock < 1) {
+            console.log({"status":"Not enough stock available","itemId":1})
+        } else {
+            console.log({"status":"Product not found"})
+        }
+    }
 })
